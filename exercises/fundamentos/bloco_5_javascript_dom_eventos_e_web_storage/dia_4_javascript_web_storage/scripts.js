b03c11b344e4606b26ccda96979c43109a2a2148
Levi Manoel <levimanoel.deob@gmail.br>
2021-07-08 00:09:07 -0300

    Web storage aplicado com sucesso hehe

--- exercises/fundamentos/bloco_5_javascript_dom_eventos_e_web_storage/dia_4_javascript_web_storage/scripts.js
+++ exercises/fundamentos/bloco_5_javascript_dom_eventos_e_web_storage/dia_4_javascript_web_storage/scripts.js
@@ -1,8 +1,22 @@
+window.onload = function() {
+  bgColor.value = localStorage.getItem('bgColorStorage');
+  changeBgColor();
+  txtColor.value = localStorage.getItem('txtColorStorage');
+  changeTxtColor();
+  txtSize.value = localStorage.getItem('txtSizeStorage');
+  changeTxtSize;
+  lineHeight.value = localStorage.getItem('lineHeightStorage');
+  changelineHeight();
+  font.value = localStorage.getItem('fontStorage');
+  changeFont();
+}
+
 //Changes the brackground color
 let bgColor = document.querySelector('#bgColor');
 document.querySelector('#bgColor').addEventListener('keyup', changeBgColor);
 function changeBgColor() {
   document.querySelector('.main').style.backgroundColor = bgColor.value;
+  localStorage.setItem('bgColorStorage', bgColor.value);
 }
 
 //Changes the text color
@@ -10,6 +24,7 @@ let txtColor = document.querySelector('#txtColor');
 document.querySelector('#txtColor').addEventListener('keyup', changeTxtColor)
 function changeTxtColor() {
   document.querySelector('.main').style.color = txtColor.value;
+  localStorage.setItem('txtColorStorage', txtColor.value);
 }
 
 //Changes the text size
@@ -17,6 +32,7 @@ let txtSize = document.querySelector('#txtSize');
 document.querySelector('#txtSize').addEventListener('keyup', changeTxtSize)
 function changeTxtSize() {
   document.querySelector('.main').style.fontSize = txtSize.value +'px';
+  localStorage.setItem('txtSizeStorage', txtSize.value);
 }
 
 //Changes the line height
@@ -24,6 +40,7 @@ let lineHeight = document.querySelector('#lineHeight');
 document.querySelector('#lineHeight').addEventListener('keyup', changelineHeight)
 function changelineHeight() {
   document.querySelector('.main').style.lineHeight = lineHeight.value +'px';
+  localStorage.setItem('lineHeightStorage', lineHeight.value);
 }
 
 //Changes the font familly
@@ -31,4 +48,12 @@ let font = document.querySelector('#font');
 document.querySelector('#font').addEventListener('keyup', changeFont);
 function changeFont() {
   document.querySelector('.main').style.fontFamily = font.value;
+  localStorage.setItem('fontStorage', font.value);
 }
+
+
+
+
+
+
+
