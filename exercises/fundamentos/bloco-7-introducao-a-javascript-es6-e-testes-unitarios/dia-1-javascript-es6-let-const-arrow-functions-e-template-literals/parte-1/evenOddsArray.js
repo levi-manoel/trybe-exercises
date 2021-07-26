665f1d1b06d507a475e497f96002b9a7cc645136
Levi Manoel <levimanoel.deob@gmail.br>
2021-07-26 18:56:42 -0300

    Exercises 7

---
+++ exercises/fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-1/evenOddsArray.js
@@ -0,0 +1,4 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const arrayOrd = oddsAndEvens.sort;
+
+console.log(`O array ordenado é: ${oddsAndEvens.sort}, valeu aí`); // será necessário alterar essa linha 😉
